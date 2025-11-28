b3a9f449ed3e60a90bb9ec922f78e0f3e5b8d406
DrakeNeJivoy <dima.kotov.23@bk.ru>
2025-11-28 20:41:12 +0500

    add open dialog

--- scripts/dialog_area.gd
+++ scripts/dialog_area.gd
@@ -1,12 +1,39 @@
 extends Area2D
 
+@onready var dialog_sign: Node = null
+var player_in_range
+#@onready var dialog_sign: Sprite2D = $"../DialogSign"
 
+var dialog_text = [
+	{
+		"name": "Drake",
+		"text": "Привет!"
+	}
+]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	dialog_sign = get_node_or_null("../DialogSign")
+	if dialog_sign:
+		dialog_sign.visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if player_in_range and Input.is_action_just_pressed("interact"):
+		GlobalSignal.emit_signal("open_dialog", dialog_text)
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if dialog_sign and body.name == 'MainChar':
+		dialog_sign.visible = true
+	else:
+		print("Неа")
+	player_in_range = true
+
+func _on_body_exited(body: Node2D) -> void:
+	if dialog_sign and body.name == 'MainChar':
+		dialog_sign.visible = false
+	else:
+		print("Неа")
+	player_in_range = false
