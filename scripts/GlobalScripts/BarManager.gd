2beffe5328fadc20f3104009c18a3e58b3565587
DrakeNeJivoy <dima.kotov.23@bk.ru>
2025-11-30 10:35:08 +0500

    Add SceenManagers

---
+++ scripts/GlobalScripts/BarManager.gd
@@ -0,0 +1,20 @@
+extends Node
+
+var door_visible
+
+signal update_door
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+func get_door_visible():
+	return door_visible
+
+func set_door_visible(chng):
+	door_visible = chng
